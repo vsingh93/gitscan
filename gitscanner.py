e6f4dc22d5b2126949fee27678c8b7b7f1d2a31b
vsingh93 <98797681+vsingh93@users.noreply.github.com>
2022-02-16 15:22:32 +0530

    Update gitscanner.py
    added some lines, removed uname

--- gitscanner.py
+++ gitscanner.py
@@ -8,39 +8,42 @@ import hvac
 
 print (f"\n\n\n{'*'*50}\nGIT SECRET SCANNING USING GITLEAKS\n{'*'*50}\n\n\n")
 
-
 slack_channel = '#testing-notifications-via-slack'
-slack_icon_emoji = ':see_no_evil:'
-slack_user_name = 'secrets_bot'
 
 def get_variables():
     ssm = boto3.client('ssm',region_name='ap-south-1')
-    response = ssm.get_parameters(Names=['gh_token','slack_token'],WithDecryption=True)
+    response = ssm.get_parameters(Names=['gh_token','slack_token','gh_uname'],WithDecryption=True)
     github_token = response['Parameters'][0]['Value']
     slack_token = response['Parameters'][1]['Value']
-    fetch_repos(github_token,slack_token)
+    gh_uname = response['Parameters'][2]['Value']
+    fetch_repos(github_token,slack_token,gh_uname)
 
-def fetch_repos(gh_token,sl_token):
+def fetch_repos(gh_token,sl_token,gh_uname):
     url = "https://api.github.com/orgs/grofers/repos"
     headers = {"Accept":"application/vnd.github.v3+json", "Authorization":"token " + gh_token}
-    payload = {'type':'all','per_page':5000}
     repo_list = []
-    try:
-        r =requests.get(url, headers=headers, params=payload)
-    except requests.exceptions.HTTPError as err:
-        post_message_to_slack(err, sl_token)
-        raise SystemExit(err)
-    print (f"Connection Successfull !!! \n\n{'*'*50}\n")
-    jsonresp = json.loads(r.text)
-    for item in jsonresp:
-        repo_list.append(item['clone_url'])
+    for i in range (1,100):
+        payload = {'type':'all','per_page':5000,'page':i}
+        try:
+            r =requests.get(url, headers=headers, params=payload)
+            print (f"\nConnection Successfull !!! Fetching page {q}. .\n")
+        except requests.exceptions.HTTPError as err:
+            post_message_to_slack(err, sl_token)
+            raise SystemExit(err)
+        if (r.content) != '[]':
+            jsonresp = json.loads(r.text)
+            for item in jsonresp:
+                repo_list.append(item['clone_url'])
+            print ("[+]Added to list !\n")
+        else:
+            exit()
     print (f"List of all available repos: \n")
     for x in repo_list:
         print (x)
     print(f"\n\n{'*'*50}\n\n")
     for item in repo_list:
         y = item.split("//")
-        url1 = "{}//vsingh93:{}@{}".format(y[0],gh_token,y[1])
+        url1 = "{}//{}:{}@{}".format(y[0],gh_uname,gh_token,y[1])
         cmd = "git clone {}".format(url1)
         os.system(cmd)
     scan_cloned_repos(sl_token)
